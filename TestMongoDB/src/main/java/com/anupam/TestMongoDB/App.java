346ff68842a67e335e0f90e611a564385363416d
Anupam <anupam.attrey@hotmail.com>
2016-01-06 22:39:59 -0500

    tttttttttttttttttttttttt

--- TestMongoDB/src/main/java/com/anupam/TestMongoDB/App.java
+++ TestMongoDB/src/main/java/com/anupam/TestMongoDB/App.java
@@ -18,16 +18,6 @@ public class App {
 
 		try {
 
-
-
-
-
-
-
-
-
-
-
 			/**** Connect to MongoDB ****/
 			// Since 2.10.0, uses MongoClient
 			MongoClient mongo = new MongoClient("localhost", 27017);
